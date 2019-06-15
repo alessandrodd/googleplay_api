3db763fd6fa06075c19d730f04578b277183de9d
alessandrodd <didiego.alessandro@gmail.com>
2019-06-15 18:59:18 +0200

    Headers should be regenerated after re-login

--- googleplay_api/googleplay.py
+++ googleplay_api/googleplay.py
@@ -265,27 +265,6 @@ class GooglePlayAPI(object):
         if datapost is None and path in self.preFetch:
             data = self.preFetch[path]
         else:
-            headers = {"Accept-Language": self.lang,
-                       "Authorization": "GoogleLogin auth={0}".format(self.authSubToken),
-                       "X-DFE-Enabled-Experiments": "cl:billing.select_add_instrument_by_default",
-                       "X-DFE-Unsupported-Experiments": "nocache:billing.use_charging_poller,"
-                                                        "market_emails,buyer_currency,prod_baseline,"
-                                                        "checkin.set_asset_paid_app_field,shekel_test,"
-                                                        "content_ratings,buyer_currency_in_app,"
-                                                        "nocache:encrypted_apk,recent_changes",
-                       "X-DFE-Device-Id": self.androidId,
-                       "X-DFE-Client-Id": "am-android-google",
-                       # "X-DFE-Logging-Id": self.loggingId2, # Deprecated?
-                       "User-Agent": user_agent,
-                       "X-DFE-SmallestScreenWidthDp": "320",
-                       "X-DFE-Filter-Level": "3",
-                       # "X-DFE-No-Prefetch": 'true',  # avoid prefetch
-                       "Accept-Encoding": "gzip, deflate",
-                       "Host": "android.clients.google.com"}
-
-            if datapost is not None:
-                headers["Content-Type"] = post_content_type
-
             url = "https://android.clients.google.com/fdfe/{0}".format(path)
             response = None
             errorRetries = self.errorRetries
@@ -293,6 +272,27 @@ class GooglePlayAPI(object):
             while retry:
                 if self.throttle:
                     sleep(self.throttleTime)
+                    
+                headers = {"Accept-Language": self.lang,
+                           "Authorization": "GoogleLogin auth={0}".format(self.authSubToken),
+                           "X-DFE-Enabled-Experiments": "cl:billing.select_add_instrument_by_default",
+                           "X-DFE-Unsupported-Experiments": "nocache:billing.use_charging_poller,"
+                                                            "market_emails,buyer_currency,prod_baseline,"
+                                                            "checkin.set_asset_paid_app_field,shekel_test,"
+                                                            "content_ratings,buyer_currency_in_app,"
+                                                            "nocache:encrypted_apk,recent_changes",
+                           "X-DFE-Device-Id": self.androidId,
+                           "X-DFE-Client-Id": "am-android-google",
+                           # "X-DFE-Logging-Id": self.loggingId2, # Deprecated?
+                           "User-Agent": user_agent,
+                           "X-DFE-SmallestScreenWidthDp": "320",
+                           "X-DFE-Filter-Level": "3",
+                           # "X-DFE-No-Prefetch": 'true',  # avoid prefetch
+                           "Accept-Encoding": "gzip, deflate",
+                           "Host": "android.clients.google.com"}
+
+                if datapost is not None:
+                    headers["Content-Type"] = post_content_type
                 if datapost is not None:
                     response = requests.post(url, data=datapost, headers=headers, verify=ssl_verify,
                                              proxies=self.proxies)
